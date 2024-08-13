3671e5e0b24cf6c48c7f4795a24dadbcca108284
Rahul Kumar <rks950408@gmail.com>
2024-08-13 16:00:47 +0530

    first commit

---
+++ Ep_1/index.js
@@ -0,0 +1,16 @@
+//template literals
+const age=20;
+const fullName="jhon doe"
+
+
+// output Heloo mr Jhon your age is :20
+console.log("Hello mr "+fullName+"your age is:"+age);
+console.log(`Hello mr ${fullName} your age is ${age}`);
+
+
+//print of sum of two  number
+
+const a=20;
+const b=30;
+const c=a+b;
+console.log(`sum is :`+c)
